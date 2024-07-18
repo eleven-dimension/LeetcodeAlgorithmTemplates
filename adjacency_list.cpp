dd673abba71dd8344856aecf8115ee2d43e82e08
eleven-dimension <3096152319@qq.com>
2024-07-18 12:08:52 +1000

    Replace int with size_t.

--- adjacency_list.cpp
+++ adjacency_list.cpp
@@ -1,24 +1,24 @@
 #include <bits/stdc++.h>
 
 struct Edge {
-    int to;
-    int weight;
+    size_t to;
+    size_t weight;
 
-    Edge(int to, int weight) : to(to), weight(weight) {}
+    Edge(size_t to, size_t weight) : to(to), weight(weight) {}
 };
 
 class Graph {
 public:
-    Graph(int number_of_vertices) : number_of_vertices_(number_of_vertices) {
+    Graph(size_t number_of_vertices) : number_of_vertices_(number_of_vertices) {
         adjacency_list_.resize(number_of_vertices);
     }
 
-    void addEdge(int u, int v, int weight) {
+    void addEdge(size_t u, size_t v, size_t weight) {
         adjacency_list_[u].emplace_back(v, weight);
         adjacency_list_[v].emplace_back(u, weight);
     }
 
-    std::vector<Edge> getOutgoingEdges(int u) {
+    std::vector<Edge> getOutgoingEdges(size_t u) {
         return adjacency_list_.at(u);
     }
 
--- floyd_warshall.cpp
+++ floyd_warshall.cpp
@@ -2,23 +2,23 @@
 
 class FloydWarshall {
 public:
-    FloydWarshall(int number_of_vertices) 
+    FloydWarshall(size_t number_of_vertices) 
         : number_of_vertices_(number_of_vertices), 
-          distance_(number_of_vertices, std::vector<int>(number_of_vertices, INF)) {
-        for (int i = 0; i < number_of_vertices; ++i) {
+          distance_(number_of_vertices, std::vector<size_t>(number_of_vertices, INF)) {
+        for (size_t i = 0; i < number_of_vertices; ++i) {
             distance_[i][i] = 0;
         }
     }
 
-    void addEdge(int from, int to, int weight) {
+    void addEdge(size_t from, size_t to, size_t weight) {
         distance_[from][to] = std::min(distance_[from][to], weight);
         distance_[to][from] = std::min(distance_[to][from], weight);
     }
 
     void computeShortestPaths() {
-        for (int k = 0; k < number_of_vertices_; ++k) {
-            for (int i = 0; i < number_of_vertices_; ++i) {
-                for (int j = 0; j < number_of_vertices_; ++j) {
+        for (size_t k = 0; k < number_of_vertices_; ++k) {
+            for (size_t i = 0; i < number_of_vertices_; ++i) {
+                for (size_t j = 0; j < number_of_vertices_; ++j) {
                     if (distance_[i][k] < INF && distance_[k][j] < INF) {
                         distance_[i][j] = std::min(distance_[i][j], distance_[i][k] + distance_[k][j]);
                     }
@@ -27,13 +27,13 @@ public:
         }
     }
 
-    int getDistance(int from, int to) const {
+    size_t getDistance(size_t from, size_t to) const {
         return distance_[from][to];
     }
 
 private:
-    int number_of_vertices_;
-    std::vector<std::vector<int>> distance_;
+    size_t number_of_vertices_;
+    std::vector<std::vector<size_t>> distance_;
 
-    static constexpr int INF = std::numeric_limits<int>::max() / 2;
+    static constexpr size_t INF = std::numeric_limits<int>::max() / 2;
 };
