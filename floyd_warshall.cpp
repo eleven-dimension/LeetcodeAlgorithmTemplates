d9ff023fabc36986792e057314a5380d3983b310
czsnb-github <czsnb@outlook.com>
2024-07-19 11:47:22 +0800

    optim floyd

--- floyd_warshall.cpp
+++ floyd_warshall.cpp
@@ -1,27 +1,36 @@
 #include <bits/stdc++.h>
 
+template <class WeightType>
 class FloydWarshall {
 public:
-    FloydWarshall(size_t number_of_vertices) 
-        : number_of_vertices_(number_of_vertices), 
-          distance_(number_of_vertices, std::vector<size_t>(number_of_vertices, INF)) {
+    FloydWarshall(size_t number_of_vertices,
+                  WeightType inf_weight = std::numeric_limits<WeightType>().max() / 2)
+        : inf_weight_(inf_weight),
+          distance_(number_of_vertices, std::vector<size_t>(number_of_vertices, inf_weight)) {
         for (size_t i = 0; i < number_of_vertices; ++i) {
             distance_[i][i] = 0;
         }
     }
 
-    void addEdge(size_t from, size_t to, size_t weight) {
+    void addDirectedEdge(size_t from, size_t to, size_t weight) {
         distance_[from][to] = std::min(distance_[from][to], weight);
-        distance_[to][from] = std::min(distance_[to][from], weight);
+    }
+
+    void addBidirectedEdge(size_t from, size_t to, size_t weight) {
+        addDirectedEdge(from, to, weight);
+        addDirectedEdge(to, from, weight);
+    }
+
+    void addEdge(size_t from, size_t to, size_t weight, bool bidirected) {
+        (bidirected ? addBidirectedEdge : addDirectedEdge)(from, to, weight);
     }
 
     void computeShortestPaths() {
-        for (size_t k = 0; k < number_of_vertices_; ++k) {
-            for (size_t i = 0; i < number_of_vertices_; ++i) {
-                for (size_t j = 0; j < number_of_vertices_; ++j) {
-                    if (distance_[i][k] < INF && distance_[k][j] < INF) {
-                        distance_[i][j] = std::min(distance_[i][j], distance_[i][k] + distance_[k][j]);
-                    }
+        const size_t num = number_of_vertices();
+        for (size_t k = 0; k < num; ++k) {
+            for (size_t i = 0; i < num; ++i) {
+                for (size_t j = 0; j < num; ++j) {
+                    distance_[i][j] = std::min(distance_[i][j], distance_[i][k] + distance_[k][j]);
                 }
             }
         }
@@ -32,8 +41,11 @@ public:
     }
 
 private:
-    size_t number_of_vertices_;
+    size_t number_of_vertices() const {
+        return distance_.size();
+    }
+
     std::vector<std::vector<size_t>> distance_;
 
-    static constexpr size_t INF = std::numeric_limits<int>::max() / 2;
+    const WeightType inf_weight_;
 };
