0e0a6a9cc043dd20b2163f91bb5532cb81c29957
eleven-dimension <3096152319@qq.com>
2024-07-29 16:31:17 +1000

    Add support for directed graphs.

--- adjacency_list.cpp
+++ adjacency_list.cpp
@@ -9,11 +9,15 @@ struct Edge {
 
 class Graph {
 public:
-    Graph(size_t number_of_vertices) : adjacency_list_(number_of_vertices) {}
+    enum class EdgeType { DIRECTED, UNDIRECTED };
+
+    Graph(size_t number_of_vertices, EdgeType type = EdgeType::UNDIRECTED) : adjacency_list_(number_of_vertices), edge_type_(type) {}
 
     void addEdge(size_t u, size_t v, size_t weight) {
         adjacency_list_[u].emplace_back(v, weight);
-        adjacency_list_[v].emplace_back(u, weight);
+        if (edge_type_ == EdgeType::UNDIRECTED) {
+            adjacency_list_[v].emplace_back(u, weight);
+        }
     }
 
     const std::vector<Edge>& getOutgoingEdges(size_t u) const {
@@ -25,5 +29,32 @@ public:
     }
 
 private:
+    EdgeType edge_type_;
     std::vector<std::vector<Edge>> adjacency_list_;
 };
+
+int main() {
+    {
+        Graph graph(3, Graph::EdgeType::DIRECTED);
+        graph.addEdge(0, 1, 1);
+        graph.addEdge(1, 2, 2);
+
+        std::cout << "Nodes adjacent to the 1-st node: ";
+        for (auto& edge : graph.getOutgoingEdges(1)) {
+            std::cout << edge.to << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    {
+        Graph graph(3);
+        graph.addEdge(0, 1, 1);
+        graph.addEdge(1, 2, 2);
+
+        std::cout << "Nodes adjacent to the 1-st node: ";
+        for (auto& edge : graph.getOutgoingEdges(1)) {
+            std::cout << edge.to << " ";
+        }
+        std::cout << std::endl;
+    }
+}
