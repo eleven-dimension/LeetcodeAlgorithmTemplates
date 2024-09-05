62512b9f580b645b7cd10bc08773bb71f0a19bcd
eleven-dimension <3096152319@qq.com>
2024-09-05 23:31:30 +1000

    Add support for `long long`.

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,48 +1,47 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <concepts>
 
 // [T, T, T, ..., T, F, F, F, ...]
 // find the first position p in [l, r] such that condition(p) is false
 #include <ranges>
 #include <optional>
 
-template <typename T1, typename T2>
-std::optional<int> FirstFalse(T1 l, T2 r, auto condition) {
-    static_assert(std::is_integral_v<T1> && std::is_integral_v<T2>, "Both l and r must be integral types");
-    int l_int = static_cast<int>(l);
-    int r_int = static_cast<int>(r);
-    if (r_int < l_int) {
+enum class BoundType {
+    Lower,
+    Upper
+};
+
+std::optional<long long> FindFirst(long long l, long long r, auto condition, BoundType bound_type) {
+    if (r < l) {
         return std::nullopt;
     }
+    auto range = std::views::iota(l, r + 1);
+    auto iter = range.end();
 
-    auto range = std::views::iota(l_int, r_int + 1);
-    auto iter = std::ranges::lower_bound(range, 0, [&](int pos, auto) {
-        return condition(pos);
-    });
+    if (bound_type == BoundType::Lower) {
+        iter = std::ranges::lower_bound(range, 0, [&](long long x, auto) {
+            return condition(x);
+        });
+    } else {
+        iter = std::ranges::upper_bound(range, 0, [&](auto, long long x) {
+            return condition(x);
+        });
+    }
+
     if (iter != range.end()) {
         return *iter;
     }
     return std::nullopt;
 }
 
-template <typename T1, typename T2>
-std::optional<int> FirstTrue(T1 l, T2 r, auto condition) {
-    static_assert(std::is_integral_v<T1> && std::is_integral_v<T2>, "Both l and r must be integral types");
-    int l_int = static_cast<int>(l);
-    int r_int = static_cast<int>(r);
-    if (r_int < l_int) {
-        return std::nullopt;
-    }
+std::optional<long long> FirstFalse(long long l, long long r, auto condition) {
+    return FindFirst(l, r, condition, BoundType::Lower);
+}
 
-    auto range = std::views::iota(l_int, r_int + 1);
-    auto iter = std::ranges::upper_bound(range, 0, [&](auto, int pos) {
-        return condition(pos);
-    });
-    if (iter != range.end()) {
-        return *iter;
-    }
-    return std::nullopt;
+std::optional<long long> FirstTrue(long long l, long long r, auto condition) {
+    return FindFirst(l, r, condition, BoundType::Upper);
 }
 
 using namespace std;
