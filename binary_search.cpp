06c8c5991816a3ccaad50102c892a2388abb79d8
eleven-dimension <3096152319@qq.com>
2024-09-05 22:57:29 +1000

    Fix `upper_bound` bug.

--- binary_search.cpp
+++ binary_search.cpp
@@ -7,9 +7,16 @@
 #include <ranges>
 #include <optional>
 
-template <typename T>
-std::optional<T> FirstFalse(T l, T r, auto condition) {
-    auto range = std::views::iota(l, r + 1);
+template <typename T1, typename T2>
+std::optional<int> FirstFalse(T1 l, T2 r, auto condition) {
+    static_assert(std::is_integral_v<T1> && std::is_integral_v<T2>, "Both l and r must be integral types");
+    int l_int = static_cast<int>(l);
+    int r_int = static_cast<int>(r);
+    if (r_int < l_int) {
+        return std::nullopt;
+    }
+
+    auto range = std::views::iota(l_int, r_int + 1);
     auto iter = std::ranges::lower_bound(range, 0, [&](int pos, auto) {
         return condition(pos);
     });
@@ -19,10 +26,17 @@ std::optional<T> FirstFalse(T l, T r, auto condition) {
     return std::nullopt;
 }
 
-template <typename T>
-std::optional<T> FirstTrue(T l, T r, auto condition) {
-    auto range = std::views::iota(l, r + 1);
-    auto iter = std::ranges::upper_bound(range, 0, [&](int pos, auto) {
+template <typename T1, typename T2>
+std::optional<int> FirstTrue(T1 l, T2 r, auto condition) {
+    static_assert(std::is_integral_v<T1> && std::is_integral_v<T2>, "Both l and r must be integral types");
+    int l_int = static_cast<int>(l);
+    int r_int = static_cast<int>(r);
+    if (r_int < l_int) {
+        return std::nullopt;
+    }
+
+    auto range = std::views::iota(l_int, r_int + 1);
+    auto iter = std::ranges::upper_bound(range, 0, [&](auto, int pos) {
         return condition(pos);
     });
     if (iter != range.end()) {
@@ -34,10 +48,40 @@ std::optional<T> FirstTrue(T l, T r, auto condition) {
 using namespace std;
 int main() {
     vector<int> f = {1, 2, 3, 4, 6};
-    auto result = FirstTrue(0, 5, [&](int pos) { return f[pos] > 10; });
-    if (result) {
-        cout << *result << endl;
-    } else {
-        cout << -1 << endl;
+
+    {
+        auto result = FirstTrue(0, 4, [&](int pos) { return f[pos] > 3; });
+        if (result) {
+            cout << *result << endl;
+        } else {
+            cout << -1 << endl;
+        }
+    }
+
+    {
+        auto result = FirstTrue(0, 4, [&](int pos) { return f[pos] > 10; });
+        if (result) {
+            cout << *result << endl;
+        } else {
+            cout << -1 << endl;
+        }
+    }
+
+    {
+        auto result = FirstFalse(0, 4, [&](int pos) { return f[pos] <= 1; });
+        if (result) {
+            cout << *result << endl;
+        } else {
+            cout << -1 << endl;
+        }
+    }
+
+    {
+        auto result = FirstFalse(0, 4, [&](int pos) { return f[pos] > 0; });
+        if (result) {
+            cout << *result << endl;
+        } else {
+            cout << -1 << endl;
+        }
     }
 }
