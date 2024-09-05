45786f9c0304a9ade38d54a6c10942bf336401fe
eleven-dimension <3096152319@qq.com>
2024-09-05 23:38:39 +1000

    Replace `\t` with spaces.

--- sparse_table.cpp
+++ sparse_table.cpp
@@ -1,53 +1,53 @@
 #include <bits/stdc++.h>
 
 namespace fast_io {
-	template<typename T>
-	inline void read(T& t) {
-		T x = 0, f = 1;
-		char c = getchar();
-		for (; !isdigit(c); c = getchar()) {
-			if (c == '-') {
-				f = -1;
-			}
-		}
-		for (; isdigit(c); c = getchar()) {
-			x = (x << 3) + (x << 1) + (c ^ '0');
-		}
-		t = x * f;
-	}
+    template<typename T>
+    inline void read(T& t) {
+        T x = 0, f = 1;
+        char c = getchar();
+        for (; !isdigit(c); c = getchar()) {
+            if (c == '-') {
+                f = -1;
+            }
+        }
+        for (; isdigit(c); c = getchar()) {
+            x = (x << 3) + (x << 1) + (c ^ '0');
+        }
+        t = x * f;
+    }
 
-	template<typename T>
-	inline void write(T x) {
-		char f[200];
-		if (x == 0) {
-			putchar('0');
-			return;
-		}
-		T tmp = x > 0 ? x : -x;
-		if (x < 0) {
-			putchar('-');
-		}
-		int cnt = 0;
-		while (tmp > 0) {
-			f[cnt++] = tmp % 10 + '0';
-			tmp /= 10;
-		}
-		while (cnt > 0) {
-			putchar(f[--cnt]);
-		}
-	}
+    template<typename T>
+    inline void write(T x) {
+        char f[200];
+        if (x == 0) {
+            putchar('0');
+            return;
+        }
+        T tmp = x > 0 ? x : -x;
+        if (x < 0) {
+            putchar('-');
+        }
+        int cnt = 0;
+        while (tmp > 0) {
+            f[cnt++] = tmp % 10 + '0';
+            tmp /= 10;
+        }
+        while (cnt > 0) {
+            putchar(f[--cnt]);
+        }
+    }
 
-	template<typename T>
-	inline void writes(T x) {
+    template<typename T>
+    inline void writes(T x) {
         write(x);
-		putchar(' ');
-	}
+        putchar(' ');
+    }
 
-	template<typename T>
-	inline void writee(T x) {
+    template<typename T>
+    inline void writee(T x) {
         write(x);
-		puts("");
-	}
+        puts("");
+    }
 }
 
 template <class T, auto op, auto e>
@@ -73,7 +73,7 @@ public:
     T Query(size_t l, size_t r) {
         assert(0 <= l && l < r && r <= n_);
         size_t len = log2(r - l);
-	    return op(table_[l][len], table_[r - (1 << len)][len]);
+        return op(table_[l][len], table_[r - (1 << len)][len]);
     }
 
 private:
@@ -93,16 +93,16 @@ int main() {
     vector<int> a(n);
     for (int i = 0; i < n; i++) {
         read(a[i]);
-	}
+    }
     auto mx = [](int a, int b) { return max(a, b); };
     auto e = []() { return -numeric_limits<int>::max(); };
     SparseTable<int, mx, e> st(a);
-	
-	for (int i = 1; i <= m; i++) {
-		int l, r;
+    
+    for (int i = 1; i <= m; i++) {
+        int l, r;
         read(l), read(r);
 
         int res = st.Query(l - 1, r);
         writee(res);
-	}
+    }
 }
