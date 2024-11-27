a0aff336f5f469c9d61f090e8543eed4d6e62a81
eleven-dimension <3096152319@qq.com>
2024-11-27 18:50:54 +0800

    Refine implementation.

--- discretizer.cpp
+++ discretizer.cpp
@@ -5,30 +5,28 @@ class Discretizer {
  public:
   Discretizer(const std::vector<T>& input) {
     sorted_ = input;
-    std::ranges::sort(sorted_);
+    std::sort(sorted_.begin(), sorted_.end());
+    sorted_.erase(std::unique(sorted_.begin(), sorted_.end()), sorted_.end());
 
-    auto it = std::unique(sorted_.begin(), sorted_.end());
-    sorted_.erase(it, sorted_.end());
-
-    for (int i = 0; i < sorted_.size(); ++i) {
-      value_to_index_[sorted_[i]] = i + 1;
-      index_to_value_[i + 1] = sorted_[i];
+    value_to_index_.reserve(sorted_.size());
+    for (size_t i = 0; i < sorted_.size(); i++) {
+      value_to_index_[sorted_[i]] = i;
     }
 
+    discrete_.reserve(input.size());
     for (const auto& value : input) {
       discrete_.emplace_back(value_to_index_[value]);
     }
   }
 
-  std::vector<int> getDiscrete() const { return discrete_; }
+  const std::vector<int>& GetDiscrete() const { return discrete_; }
 
-  int toDiscrete(const T& value) const { return value_to_index_.at(value); }
+  int ToDiscrete(const T& value) const { return value_to_index_.at(value); }
 
-  T toOriginal(int index) const { return index_to_value_.at(index); }
+  const T& ToOriginal(int index) const { return sorted_.at(index); }
 
  private:
   std::vector<T> sorted_;
   std::unordered_map<T, int> value_to_index_;
-  std::unordered_map<int, T> index_to_value_;
   std::vector<int> discrete_;
 };
