36c5b961c026203bfe6e3849c96b804b8e7bf86d
eleven-dimension <3096152319@qq.com>
2024-12-16 10:50:31 +1100

    Add `LastTrue` & `LastFalse`.

--- binary_search.cpp
+++ binary_search.cpp
@@ -9,35 +9,68 @@
 #include <ranges>
 
 enum class BoundType { Lower, Upper };
+enum class TargetPosition { First, Last };
 
-std::optional<long long> FindFirst(long long l, long long r, auto condition,
-                                   BoundType bound_type) {
+std::optional<long long> FindBound(long long l, long long r,
+                                   const auto& condition, BoundType bound_type,
+                                   TargetPosition target_position) {
   if (r < l) {
     return std::nullopt;
   }
   auto range = std::views::iota(l, r + 1);
-  auto iter = range.end();
+  auto iter =
+      (target_position == TargetPosition::First ? range.end() : range.begin());
+  bool is_upper_bound = (target_position == TargetPosition::Last &&
+                         bound_type == BoundType::Lower) ||
+                        (target_position == TargetPosition::First &&
+                         bound_type == BoundType::Upper);
+  auto func = [is_upper_bound](auto&&... args) {
+    if (is_upper_bound)
+      return std::ranges::upper_bound(std::forward<decltype(args)>(args)...);
+    else
+      return std::ranges::lower_bound(std::forward<decltype(args)>(args)...);
+  };
 
-  if (bound_type == BoundType::Lower) {
-    iter = std::ranges::lower_bound(
-        range, 0, [&](long long x, auto) { return condition(x); });
-  } else {
-    iter = std::ranges::upper_bound(
-        range, 0, [&](auto, long long x) { return condition(x); });
+  auto reversed_condition = [&](long long x) {
+    return target_position == TargetPosition::Last ? !condition(x)
+                                                   : condition(x);
+  };
+
+  auto comparator = [&](auto a, auto b) {
+    auto x = is_upper_bound ? b : a;
+    return reversed_condition(x);
+  };
+
+  iter = func(range, 0, comparator);
+
+  if (target_position == TargetPosition::Last && iter != range.begin()) {
+    --iter;
+    return *iter;
   }
-
-  if (iter != range.end()) {
+  if (target_position == TargetPosition::First && iter != range.end()) {
     return *iter;
   }
   return std::nullopt;
 }
 
-std::optional<long long> FirstFalse(long long l, long long r, auto condition) {
-  return FindFirst(l, r, condition, BoundType::Lower);
+std::optional<long long> FirstFalse(long long l, long long r,
+                                    const auto& condition) {
+  return FindBound(l, r, condition, BoundType::Lower, TargetPosition::First);
 }
 
-std::optional<long long> FirstTrue(long long l, long long r, auto condition) {
-  return FindFirst(l, r, condition, BoundType::Upper);
+std::optional<long long> FirstTrue(long long l, long long r,
+                                   const auto& condition) {
+  return FindBound(l, r, condition, BoundType::Upper, TargetPosition::First);
+}
+
+std::optional<long long> LastFalse(long long l, long long r,
+                                   const auto& condition) {
+  return FindBound(l, r, condition, BoundType::Upper, TargetPosition::Last);
+}
+
+std::optional<long long> LastTrue(long long l, long long r,
+                                  const auto& condition) {
+  return FindBound(l, r, condition, BoundType::Lower, TargetPosition::Last);
 }
 
 using namespace std;
